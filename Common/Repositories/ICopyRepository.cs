611972aa64215a06aa27a595c44a245a77db13f0
Melusine Chrs <melusine.chrs@gmail.com>
2025-03-13 16:23:23 +0100

    Added missing repositories

---
+++ Common/Repositories/ICopyRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Repositories
+{
+    interface ICopyRepository<TCopy>
+    {
+        Guid Insert(TCopy copy);
+        TCopy Get(Guid copy_id);
+        void Update(Guid copy_id, TCopy copy);
+        void Delete(Guid copy_id);
+    }
+}
---
+++ Common/Repositories/ITagRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Repositories
+{
+    public interface ITagRepository<TTag>
+    {
+        Guid Insert(TTag tag_id);
+        TTag Get(Guid tag_id);
+    }
+}
